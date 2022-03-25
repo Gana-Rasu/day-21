565f9dec37298389fc7c48ca0b785389029e2bdb
Gananathan <92508504+Gana-Rasu@users.noreply.github.com>
2022-03-25 10:45:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+const display = document.querySelector(".display");
+
+
+setTimeout(()=>{
+    // console.log("10");
+    display.innerHTML = "10"
+    setTimeout(()=>{
+        display.innerHTML = "9"
+        setTimeout(()=>{
+            display.innerHTML = "8"
+            setTimeout(()=>{
+                display.innerHTML = "7"
+                setTimeout(()=>{
+                    display.innerHTML = "6"
+                    setTimeout(()=>{
+                        display.innerHTML = "5"
+                        setTimeout(()=>{
+                            display.innerHTML = "4"
+                            setTimeout(()=>{
+                                display.innerHTML = "3"
+                                setTimeout(()=>{
+                                    display.innerHTML = "2"
+                                    setTimeout(()=>{
+                                        display.innerHTML = "1"
+                                        setTimeout(()=>{
+                                            display.innerHTML = "HAPPY INDEPENDANCE DAY"
+                                        },2000)
+                                    },2000)
+                                },2000)
+                            },2000)
+                        },2000)
+                    },2000)
+                },2000)
+            },2000)
+        },2000)
+    },2000)
+},2000)
